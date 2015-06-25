b3b088fd532031a344052286e69af1a45a92119e
pommepoco <hugo.arru@epitech.eu>
2015-06-25 16:42:24 +0200

    comment article post

--- public/controllers/blogsCtrl.js
+++ public/controllers/blogsCtrl.js
@@ -6,12 +6,22 @@ module.controller("BlogsCtrl", [
   function ($scope, $http, $location, $rootScope) {
     console.log("blogCtrl");
     $rootScope.host = window.location.host;
+
+    // Get all blogs
     $http.get("/blog")
       .success(function(data) {
         $scope.blogs = data.blogs;
         console.log($scope.blogs);
       }).error(function() {
         alert("une erreur c'est produite");
+      });
+
+    // Add blog
+    $http.post("/blog", $scope.newBlog)
+      .success(function(data) {
+        console.log(data);
+      }).error(function(data) {
+        alert("Une erreur est survenu");
       });
   }
 ]);
