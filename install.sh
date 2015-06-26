e5d33fdad66c015cb06e4ea655129cd6d1e7db67
pommepoco <hugo.arru@epitech.eu>
2015-06-26 11:26:17 +0200

    install

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get install vim nodejs npm mongodb redis-server
-sudo echo "127.0.0.1 test.localhost" >> /etc/hosts
-sudo echo "127.0.0.1 myblog.localhost" >> /etc/hosts
-sudo echo "127.0.0.1 newblog.localhost" >> /etc/hosts
-sudo npm install -g forever
+su
+apt-get update
+apt-get install vim nodejs npm mongodb redis-server
+echo "127.0.0.1 test.localhost" >> /etc/hosts
+echo "127.0.0.1 myblog.localhost" >> /etc/hosts
+echo "127.0.0.1 newblog.localhost" >> /etc/hosts
+npm install -g forever
 redis-server --daemonize yes
 mkdir -p data/db
 mongod mongod --fork --logpath ./mongodb.log --dbpath data/db
-sudo npm install -g bower forever
+npm install -g bower forever
 npm install
 bower install
 forever ./bin/www
