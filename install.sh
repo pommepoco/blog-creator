deaf686864cc75281daa7a65b4c5be2a353cfa6f
pommepoco <hugo.arru@epitech.eu>
2015-06-26 10:26:46 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
+
 sudo apt-get install vim node mongodb redis
 sudo echo "127.0.0.1 test.localhost" >> /etc/hosts
 sudo echo "127.0.0.1 myblog.localhost" >> /etc/hosts
 sudo echo "127.0.0.1 newblog.localhost" >> /etc/hosts
-git clone git@github.com:pommepoco/blog-creator.git
-cd blog-creator
 sudo npm install -g forever
 redis-server --daemonize yes
 mkdir -p data/db
 mongod mongod --fork --logpath ./mongodb.log --dbpath data/db
+npm install -g bower
 npm install
 bower install
 forever ./bin/www
